c1000118a0e89831ed0aebd54b36324a1fe1fc52
hamadaharu <hamada.haru@gmail.com>
2025-06-28 00:56:23 +0900

    clean:不要なタグ、クラス名などの整理

--- database/app.js
+++ database/app.js
@@ -1,34 +1,25 @@
-
-
 (() => {
 
   const initialEmail = localStorage.getItem('E-mail');
   const initialPassword = localStorage.getItem('Password');
 
-  let email = document.getElementById('email');
-  let pass = document.getElementById('pass');
+  let $email = document.getElementById('email');
+  let $pass = document.getElementById('pass');
 
-  email.value = initialEmail;
-  pass.value = initialPassword;
+  $email.value = initialEmail;
+  $pass.value = initialPassword;
 
   function buttonClick(){
-    // msg.innerText = '登録アドレスは' + email.value + 'パスワードは' + pass.value +'ですね';
-    // console.log(email.value, pass.value);
-
     if(('localStorage' in window) && (window.localStorage !== null)){
-      localStorage.setItem('E-mail', email.value);
-      localStorage.setItem('Password', pass.value);
+      localStorage.setItem('E-mail', $email.value);
+      localStorage.setItem('Password', $pass.value);
 
     } else {
       return;
     }
   }
 
-  // let msg = document.getElementById('Page_msg');
-
-  let signInButton = document.getElementById('signIn');
-  signInButton.addEventListener('click', buttonClick);
-
+  let $signInButton = document.getElementById('signIn');
+  $signInButton.addEventListener('click', buttonClick);
 
 })();
-
